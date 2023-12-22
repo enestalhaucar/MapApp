44157382d3f0ab02d9619e91137e006ac139e33f
Enes Talha Uçar <talhauenes@gmail.com>
2023-12-22 14:13:45 +0300

    Added Location Details View

---
+++ MapAppSwiftUI/Views/LocationDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailView.swift
+//  MapAppSwiftUI
+//
+//  Created by Enes Talha Uçar  on 22.12.2023.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailView()
+}
