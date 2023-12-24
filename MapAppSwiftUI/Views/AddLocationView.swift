97452bbd31ad3b5fdf0198b454877baa7297eac4
Enes Talha Uçar <talhauenes@gmail.com>
2023-12-24 11:59:19 +0300

    Added "AddLocationView"

---
+++ MapAppSwiftUI/Views/AddLocationView.swift
@@ -0,0 +1,18 @@
+//
+//  AddLocationView.swift
+//  MapAppSwiftUI
+//
+//  Created by Enes Talha Uçar  on 23.12.2023.
+//
+
+import SwiftUI
+
+struct AddLocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddLocationView()
+}
